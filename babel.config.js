fde53e20bc027de7139eba62b6d9c0dde2507d7b
Thomas Reggi <thomas@reggi.com>
2021-12-07 22:12:35 -0500

    typescript

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     presets: ['@expo/next-adapter/babel'],
     plugins: [
+        'babel-plugin-transform-typescript-metadata',
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        'babel-plugin-parameter-decorator',
         ["@babel/plugin-proposal-private-methods", { "loose": true }],
         ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
     ]
